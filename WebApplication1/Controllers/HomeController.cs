6f35092395883211cf1391e2e85566830b5f06c1
Ba1nky <95933124+Ba1nky@users.noreply.github.com>
2022-09-30 12:20:17 +0300

    Add files via upload

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        StudentMark [] studentMarkArray = { 
+            new StudentMark {
+                FirstName = "Cергій",
+                LastName = "Гаврилюк",
+                FatherName = "Іванович",
+                Mark = 4,
+                Subject = "Громадянська освіта",
+                Date = DateTime.Now.AddDays(-20).AddHours(-3)
+            },
+            new StudentMark {
+                FirstName = "Андрій",
+                LastName = "Карпенко",
+                FatherName = "Миколайович",
+                Mark = 2,
+                Subject = "ТІМС",
+                Date = DateTime.Now.AddDays(-11).AddHours(17)
+            },
+            new StudentMark {
+                FirstName = "Микола",
+                LastName = "Горбатий",
+                FatherName = "Юрійович",
+                Mark = 5,
+                Subject = "Програмування на Python",
+                Date = DateTime.Now.AddDays(-15).AddHours(5)
+            },
+            new StudentMark {
+                FirstName = "Дмитро",
+                LastName = "Хомяк",
+                FatherName = "Олександрович",
+                Mark = 1,
+                Subject = "Вища математика",
+                Date = DateTime.Now.AddDays(-17).AddHours(-2)
+            },
+            new StudentMark {
+                FirstName = "Геннадій", 
+                LastName = "Тимчеко",
+                FatherName = "Вікторович",
+                Mark = 3,
+                Subject = "Програмування на С++",
+                Date = DateTime.Now.AddDays(-8).AddHours(-18)
+            }
+        };
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(studentMarkArray);
+        }
+
+        [HttpGet("below_three")]
+        public ActionResult BelowThree()
+        {
+            return View(studentMarkArray);
+        }
+    }
+}
---
+++ WebApplication1/Models/StudentMark.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Models
+{
+    public class StudentMark
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string FatherName { get; set; } = null!;
+        public int Mark { get; set; }
+        public string Subject { get; set; } = null!;
+        public DateTime Date { get; set; }
+
+        public string GetName()
+        {
+            return LastName + " " + FirstName + " " + FatherName;
+        }
+    }
+}
